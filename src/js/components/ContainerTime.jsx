39859bc37c69f8fe3071cd636977fcf3214476dc
Carlos Melchor Perez <carloos0665@gmail.com>
2025-03-15 01:14:29 +0000

    add SetInterval

--- src/js/components/ContainerTime.jsx
+++ src/js/components/ContainerTime.jsx
@@ -1,10 +1,18 @@
 import React from "react";
 
 export const ContainerTime = () => {
+
+    let count = 0;
+    setInterval(() => {
+        count = count + 1
+        document.getElementById("Count").innerText = count
+    }, 1000)
     return (
-        <div
+        <div id="Count"
             className="d-flex justify-content-center align-items-center text-white mx-4"
             style={{ height: "20vh", width: "13vh", borderRadius: "2vh", border: " 5px solid gray" }}
-        ></div>
+        >
+            0
+        </div>
     )
 }
--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 import { ContainerTime } from "./ContainerTime";
 
 //create your first component
+
+
 export const Home = () => {
 	return (
 		<div className="container-fluid">
@@ -11,13 +12,7 @@ export const Home = () => {
 				<div className="bg-dark mt-5 d-flex justify-content-center align-items-center mx-auto"
 					style={{ height: "25vh", maxWidth: "90vw", borderRadius: "2vh" }}>
 					<div className="d-flex justify-content-center">
-						<ContainerTime/>
-						<ContainerTime/>
-						<ContainerTime/>
-						<ContainerTime/>
-						<ContainerTime/>
-						<ContainerTime/>
-						<ContainerTime/>
+						<ContainerTime />
 					</div>
 				</div>
 			</div>
